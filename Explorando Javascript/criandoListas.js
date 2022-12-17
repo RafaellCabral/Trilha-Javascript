f0f04472143c83e77d9de687f8ba1c0ed3d9c866
rafael cabral <rc.rafaelcabral@hotmail.com>
2022-12-16 22:48:52 -0300

    novos

---
+++ Explorando Javascript/criandoListas.js
@@ -0,0 +1,15 @@
+console.log(`Trabalhando com listas`);
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+
+);
+
+listaDeDestinos.push('Curitiba', ' Floripa', 'Foz do Iguaçu', 'Fortaleza');
+// listaDeDestinos.splice(0, 2);  vai remover um item que eu quero
+console.log(listaDeDestinos);
